f6242be031569e9200fc2ee632fe62b3919fdf0b
Alexander Mills <spot.enterprise@web.de>
2024-12-01 22:20:02 +0100

    Finished Task 1

--- tasks/task1.py
+++ tasks/task1.py
@@ -6,10 +6,13 @@ class Task1(AbstractTask):
         super().__init__(1)
     
     def simple_task(self):
-        pass
+        l1, l2 = list(zip(*[list(map(int, x.split('   '))) for x in self.read_file_lines()]))
+        return sum([abs(x-y) for x,y in zip(sorted(l1), sorted(l2))])
+
     
     def extended_task(self):
-        pass
+        l1, l2 = list(zip(*[list(map(int, x.split('   '))) for x in self.read_file_lines()]))
+        return sum([x * l2.count(x) for x in l1])
 
 
 Task1()
