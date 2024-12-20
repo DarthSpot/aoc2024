d23ff272655c70a52fda5bc6e05a54991a969cba
Alexander Mills <spot.enterprise@web.de>
2024-12-20 08:30:30 +0100

    Day 10

--- tasks/task10.py
+++ tasks/task10.py
@@ -1,15 +1,51 @@
+from collections import defaultdict
+
 from tasks.abstracttask import AbstractTask
 
 
 class Task10(AbstractTask):
     def __init__(self):
         super().__init__(10)
-    
+
+    def get_neighbors(self, map, x: int, y: int) -> list:
+        results = []
+        for nx, ny in [(0,1), (0,-1), (1,0), (-1,0)]:
+            px, py = x + nx, y + ny
+            if (px,py) in map:
+                results.append((px, py))
+        return results
+
+    def find_trails(self):
+        input = self.read_file_lines()
+        zeros = defaultdict(list)
+        tops = []
+        trail_map = {}
+        for idy, line in enumerate(input):
+            for idx, c in enumerate(line):
+                h = int(c)
+                trail_map[(idx, idy)] = h
+                if h == 9:
+                    tops.append((idx, idy))
+        for top in tops:
+            trails = [top]
+            while len(trails) > 0:
+                trail, *trails = trails
+                x, y = trail
+                h = trail_map[(x, y)]
+                if h > 0:
+                    trails = trails + [(nx, ny) for nx, ny in self.get_neighbors(trail_map, x, y) if
+                                       trail_map[(nx, ny)] == h - 1]
+                else:
+                    zeros[(x, y)].append(top)
+        return zeros
+
     def simple_task(self):
-        pass
+        return sum([len(set(x)) for k,x in self.find_trails().items()])
+
+
     
     def extended_task(self):
-        pass
+        return sum([len(x) for k, x in self.find_trails().items()])
 
 
 Task10()
--- tasks/task8.py
+++ tasks/task8.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from tasks.abstracttask import AbstractTask
 
 
@@ -6,10 +8,59 @@ class Task8(AbstractTask):
         super().__init__(8)
     
     def simple_task(self):
-        pass
-    
+        input = self.read_file_lines()
+        antenna_map = defaultdict(list)
+        height = len(input)
+        width = len(input[0])
+        for idy, line in enumerate(input):
+            for idx, c in enumerate(line):
+                if c != '.':
+                    antenna_map[c].append((idx, idy))
+        results = []
+        for key, positions in antenna_map.items():
+            while len(positions) > 1:
+                (hx, hy), *tail = positions
+                for tx, ty in tail:
+                    dx, dy = tx - hx, ty - hx
+                    for x,y in [(x,y) for x,y in [(tx + dx, ty + dy), (hx - dx, hy - dy)] if 0 <= x < width and 0 <= y < height]:
+                        results.append((x,y))
+                positions = tail
+
+        return len(set(results))
+
     def extended_task(self):
-        pass
+        input = self.read_file_lines()
+        antenna_map = defaultdict(list)
+        height = len(input)
+        width = len(input[0])
+        for idy, line in enumerate(input):
+            for idx, c in enumerate(line):
+                if c != '.':
+                    antenna_map[c].append((idx, idy))
+        results = []
+        for key, positions in antenna_map.items():
+            while len(positions) > 1:
+                (hx, hy), *tail = positions
+                for tx, ty in tail:
+                    dx, dy = tx - hx, ty - hx
+                    harmonic = 0
+                    hdx, hdy = hx, hy
+                    tdx, tdy = tx, ty
+                    all_in_map = True
+                    while all_in_map:
+                        h_in_map = 0 <= hdx < width and 0 <= hdy < height
+                        t_in_map = 0 <= tdx < width and 0 <= tdy < height
+                        if h_in_map:
+                            hdx, hdy = hdx - dx * harmonic, hdy - dy * harmonic
+                            results.append((hdx, hdy))
+                        if t_in_map:
+                            tdx, tdy = tdx + dx * harmonic, tdy + dy * harmonic
+                            results.append((tdx, tdy))
+                        all_in_map = h_in_map or t_in_map
+                        harmonic += 1
+                positions = tail
+
+        return len(set(results))
 
 
 Task8()
